ffeb453bf5175c767379ece95748c5d1ddec3623
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-19 21:25:49 +0530

    Initialized Colorbox component

---
+++ src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ColorBox extends Component{
+  render(){
+    return(
+      <div className="ColorBox">
+
+      </div>
+
+    )
+  }
+}
+export default ColorBox;
