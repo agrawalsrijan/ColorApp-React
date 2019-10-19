8466577dc09d016793af84ab9a6908aab349b603
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-19 21:20:26 +0530

    Created the Pallete component
    Added seedColors that contains all colors

---
+++ src/Pallete.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Pallete extends Component{
+  render(){
+    return(
+      <div className="Pallete">
+        {/* navbar goes here */}
+        <div
+          className="pallete-colors">{/*bunch of color boxes*/}
+        </div>
+        {/*footer eventually*/}
+
+      </div>
+
+    )
+  }
+}
+export default Pallete;
