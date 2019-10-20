9e7854373672d3ca80fe130d5fffb3539a3bf178
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-20 13:26:56 +0530

    Adding ColorBox component
    Adding styling for colorbox and pallete.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-
+import "./ColorBox.css";
 class ColorBox extends Component{
   render(){
     return(
-      <div className="ColorBox">
-
+      <div style={{background:this.props.background}} className="ColorBox">
+        <span>{this.props.name}</span>
+        <span>more</span>
       </div>
 
     )
--- src/Pallete.js
+++ src/Pallete.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
-
+import ColorBox from "./ColorBox";
+import "./Pallete.css";
 class Pallete extends Component{
   render(){
+    const colorBoxes = this.props.colors.map(color=>(
+      <ColorBox background={color.color} name={color.name}/>
+    ));
     return(
       <div className="Pallete">
         {/* navbar goes here */}
-        <div
-          className="pallete-colors">{/*bunch of color boxes*/}
+        <div className="Pallete-colors">
+          {/*bunch of color boxes*/}
+          {colorBoxes}
         </div>
         {/*footer eventually*/}
 
