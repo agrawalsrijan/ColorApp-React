c9e59aad8580ec6216782fb31472a3a7ed42d944
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-21 20:02:49 +0530

    Adding color slider

--- src/Pallete.js
+++ src/Pallete.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
 import ColorBox from "./ColorBox";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 import "./Pallete.css";
 class Pallete extends Component{
+  constructor(props){
+    super(props);
+    this.state = {level:500}
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(newLevel){
+    this.setState({level:newLevel})
+  }
   render(){
-    const colorBoxes = this.props.pallete.colors[800].map(color=>(
+    const {colors} = this.props.pallete;
+    const {level} = this.state;
+    const colorBoxes = colors[level].map(color=>(
       <ColorBox background={color.hex} name={color.name}/>
     ));
     return(
       <div className="Pallete">
+
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
+
         {/* navbar goes here */}
         <div className="Pallete-colors">
           {/*bunch of color boxes*/}
