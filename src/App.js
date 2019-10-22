1593af3b9b5425faa90c68a47a8bffc679f1213d
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-22 14:33:08 +0530

    Adding react-router -- Initializing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
 import Pallete from "./Pallete";
+import {Route,Switch} from "react-router-dom";
 import {generatePallete} from "./colorHelper";
 import seedColors from "./seedColors";
 
 class App extends Component {
   render(){
-    
+
     return (
-      <div>
-        <Pallete pallete={generatePallete(seedColors[4])}/>
-      </div>
+      <Switch>
+
+        <Route
+          exact
+          path="/"
+          render={()=> <h1>Pallete list goes here</h1>}
+        />
+
+        <Route
+          exact
+          path="/pallete/:id"
+          render={()=> <h1>Individual pallete goes here</h1>}
+        />
+      </Switch>
+
+      // <div>
+      //   <Pallete pallete={generatePallete(seedColors[4])}/>
+      // </div>
     );
   }
 
