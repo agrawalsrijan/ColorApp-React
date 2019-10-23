530c758b07f34d5864def2eecb337861b1e915c8
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-23 14:49:53 +0530

    Start Styling PalleteList using JSS withStyles

--- src/PalleteList.js
+++ src/PalleteList.js
@@ -1,23 +1,61 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import {withStyles} from "@material-ui/styles";
 import MiniPallete from "./MiniPallete";
+
+// Add JSS withStyles
+const styles = {
+  root:{
+    backgroundColor:"blue",
+    height:"100%",
+    display:"flex",
+    alignItems:"flex-start",
+    justifyContent:"center"
+  },
+  container:{
+    width:"50%",
+    display:"flex",
+    alignItems:"flex-start",
+    flexDirection:"column",
+    flexWrap:"wrap"
+  },
+  nav:{
+    display:"flex",
+    width:"100%",
+    justifyContent:"space-between",
+    color:"white"
+  },
+  palletes:{
+    boxSizing:"border-box",
+    width:"100%",
+    display:"grid",
+    gridTemplateColumns:"repeat(3, 30%)",
+    gridGap:"5%"
+  }
+};
+
+
+
 
 class PalleteList extends Component{
 
   render(){
-    const {palletes} = this.props;
+    const {palletes,classes} = this.props;
     return(
 
-      <div>
-        <MiniPallete/>
-        <h1>React Colors!</h1>
-        {palletes.map(pallete=>(
-          <MiniPallete {...pallete}/>
-
-
-        ))}
+      <div className = {classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors!</h1>
+          </nav>
+          <div className={classes.palletes}>
+            {palletes.map(pallete=>(
+              <MiniPallete {...pallete}/>
+            ))}
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
-export default PalleteList;
+export default withStyles(styles)(PalleteList);
