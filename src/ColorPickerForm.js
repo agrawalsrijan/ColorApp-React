1e01fd4431aadfdb89ec0508dddd83544677300d
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-11-14 12:19:01 +0530

    Moving JSS styles out
    Moved Most JSS styles of components to seperate files.

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -3,23 +3,9 @@ import { withStyles } from '@material-ui/core/styles';
 import {ValidatorForm, TextValidator} from "react-material-ui-form-validator";
 import {ChromePicker} from "react-color";
 import Button from "@material-ui/core/Button";
+import styles from "./styles/ColorPickerFormStyles";
 
-const styles = {
-  picker:{
-    width:"100% !important",
-    marginTop:"2rem"
-  },
-  addColor:{
-    width:"100%",
-    padding:"1rem",
-    marginTop:"1rem",
-    fontSize:"1.5rem"
-  },
-  colorNameInput:{
-    width:"100%",
-    height:"70px"
-  }
-}
+
 class ColorPickerForm extends Component{
   constructor(props){
     super(props);
--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,38 +2,8 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {SortableElement} from "react-sortable-hoc";
 import DeleteIcon from "@material-ui/icons/Delete";
+import styles from "./styles/DraggableColorBoxStyles";
 
-const styles = {
-  root:{
-    width: "20%",
-    height:"25%",
-    margin:"0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-4.5px",
-    "&:hover svg":{
-      color:"white",
-      transform:"scale(1.4)"
-    }
-  },
-  boxContent:{
-    position: "absolute",
-    width: "100%",
-    left: "0px",
-    bottom: "0px",
-    padding:"10px",
-    color: "rgba(0, 0, 0, 0.7)",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px",
-    display:"flex",
-    justifyContent:"space-between"
-  },
-  deleteIcon:{
-    transition:"all 0.3s ease-in-out"
-  }
-}
 
 const DraggableColorBox = SortableElement(props => {
   const {classes,handleClick,name,color} = props;
