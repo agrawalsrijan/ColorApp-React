5cc08512b3f4efb39e6915a7e237743ef762d92b
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-11-14 12:34:12 +0530

    Tweaking Styles of NewPalleteForm

--- src/PalleteFormNav.js
+++ src/PalleteFormNav.js
@@ -9,6 +9,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 import Button from "@material-ui/core/Button";
 import styles from "./styles/PalleteFormNavStyles";
 
@@ -47,7 +48,7 @@ class PalleteFormNav extends Component{
               onClick={this.props.handleDrawerOpen}
               className={classNames(classes.menuButton, open && classes.hide)}
             >
-              <MenuIcon />
+              <AddToPhotosIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
               Create A Pallete
