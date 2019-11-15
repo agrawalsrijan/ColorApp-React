f6c5e210d36d6bf1e0ec1dc31219a4fa816ca8b6
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-11-15 21:10:25 +0530

    Making PalleteList responsive

--- src/styles/PalleteListStyles.js
+++ src/styles/PalleteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 // Add JSS withStyles
 export default {
   root:{
@@ -12,7 +13,13 @@ export default {
     display:"flex",
     alignItems:"flex-start",
     flexDirection:"column",
-    flexWrap:"wrap"
+    flexWrap:"wrap",
+    [sizes.down("xl")]:{
+      width:"80%"
+    },
+    [sizes.down("xs")]:{
+      width:"75%"
+    }
   },
   nav:{
     display:"flex",
@@ -31,6 +38,13 @@ export default {
     width:"100%",
     display:"grid",
     gridTemplateColumns:"repeat(3, 30%)",
-    gridGap:"5%"
+    gridGap:"2.5rem",
+    [sizes.down("md")]:{
+      gridTemplateColumns:"repeat(2, 50%)",
+    },
+    [sizes.down("xs")]:{
+      gridTemplateColumns:"repeat(1, 100%)",
+      gridGap:"1rem"
+    }
   }
 };
