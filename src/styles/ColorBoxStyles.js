ea86477167ddcd3d4531ac3c835f2fa9806f1a48
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-11-15 13:24:27 +0530

    Add a helper file "size.js" to make pages responsive
    The responsive helper file- "size.js" return media queries depending on the screen size to help make the pages responsive.

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,5 +1,5 @@
 import chroma from "chroma-js";
-
+import sizes from "./sizes";
 export default {
   ColorBox:{
     height: props => (props.showingFullPallete? "25%" : "50%"),
@@ -11,6 +11,18 @@ export default {
     marginBottom: "-4.5px",
     "&:hover button": {
       opacity: "1"
+    },
+    [sizes.down("lg")]:{
+      width:"25%",
+      height: props => (props.showingFullPallete? "20%" : "50%")
+    },
+    [sizes.down("md")]:{
+      width:"50%",
+      height: props => (props.showingFullPallete? "10%" : "50%")
+    },
+    [sizes.down("xs")]:{
+      width:"100%",
+      height: props => (props.showingFullPallete? "5%" : "10%")
     }
   },
   copyText:{
